0b5104165b55f927d15213cd526c9b1f1807f4c8
Antoine Salesse <antoinesalesse@gmail.fr>
2022-08-31 08:19:52 +0200

    stable ?

--- buildSrc/src/main/kotlin/fr.tawane.kotlin-springboot-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/fr.tawane.kotlin-springboot-conventions.gradle.kts
@@ -1,4 +1,11 @@
 plugins {
-    id("fr.tawane-springboot-conventions")
-    `java-library`
+    java
+    id("fr.tawane.kotlin-common-conventions")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
--- modules/booking/bootstrap/build.gradle.kts
+++ modules/booking/bootstrap/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("fr.tawane.kotlin-springboot-application-conventions")
+    id("org.springframework.boot")
+    java
+    application
 }
 
 dependencies {
@@ -7,4 +10,9 @@ dependencies {
     api(project(":modules:booking:vsa-acl"))
     api(project(":modules:booking:persistence-adapters"))
     api(project(":modules:booking:provider-adapters"))
+    implementation(libs.springBootGradlePlugin)
+}
+
+bootJar {
+    mainClassName = "com.baeldung.Application"
 }
